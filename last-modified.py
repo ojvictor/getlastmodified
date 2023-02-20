2d3ed438e0f5136016656e24334cea63422868a8
Joao Barbara <jvictor32@gmail.com>
2023-02-20 16:49:49 -0300

    docker mariadb database container

--- last-modified.py
+++ last-modified.py
@@ -4,6 +4,7 @@ import json
 from time import gmtime, strftime
 from urllib.parse import urlparse
 from datetime import datetime
+import mysql.connector
 
 def convert_str2date(strdate):
     return datetime.strptime(strdate.replace(' GMT', ''), "%a, %d %b %Y %H:%M:%S").date()
@@ -11,7 +12,8 @@ def convert_str2date(strdate):
 def get_date(): return strftime("%Y-%m-%d", gmtime())
 
 def get_last_modified(url):
-    page = {'url': "", 'date': "", 'last_modified': "", 'error': False}
+    #page = {'url': "", 'date': "", 'last_modified': "", 'error': False}
+    page = {}
     try: 
         result = urlparse(url)
         if [result.scheme, result.netloc, result.path]:
@@ -20,18 +22,35 @@ def get_last_modified(url):
                 page["url"] = url
                 page["date"] = get_date()
                 page["last_modified"] = convert_str2date(header['Last-Modified'])
-                page["error"] = False
+                page["error"] = 0
     except Exception:
         page["url"] = url
         page["date"] = get_date()
-        page["last_modified"] = None
-        page["error"] = True
-        
-    return json.dumps(page, indent = 2, default = str)
+        page["error"] = 1
+    
+    return page
+    #return json.dumps(page, indent = 2, default = str)
 if __name__ == '__main__':
     try:
         url = sys.argv[1]
         print(get_last_modified(url))
+        cnx = mysql.connector.connect(user='ticweb', database='ticweb_a11y', port='6033', password='K4RN4Vu')
+        cursor = cnx.cursor()
+
+        page = get_last_modified(url)
+
+        columns = ', '.join("`" + str(x).replace('/', '_') + "`" for x in page.keys())
+        values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in page.values())
+
+        add_page = "INSERT INTO %s ( %s ) VALUES ( %s );" % ('pages', columns, values)
+        print(add_page)
+
+        cursor.execute(add_page, page.values())
+
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+
     except IndexError: 
         print("Please provide proper url as a first argument.")
     except ValueError:
